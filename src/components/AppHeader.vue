2a2898648b8933879246767b6224e8bfa29c55cb
alessandra martera <ale.martera@gmail.com>
2023-10-27 22:05:28 +0200

    finish header and starting name in jumbotrone

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,15 +8,134 @@ export default {
 
 <template>
   <header>
-    ciao
+
+    <!-- logo -->
+    <div>
+      Alessandra
+    </div>
+
+    <!-- nav list -->
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Portfolio</a></li>
+        <li><a href="">About me</a></li>
+        <li><a href="">Pages</a></li>
+      </ul>
+
+      <!-- contact me -->
+      <div>
+        Contact |
+        <a href=""><i class="fa-brands fa-linkedin"></i></a>
+        <a href=""><i class="fa-brands fa-github"></i></a>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+      </div>
+    </nav>
   </header>
+  <!-- jumbotrone -->
+  <div id="hero">
+    <div class="center_container">
+      <section>
+        <div id="name">
+          Alessandra <br>
+          Martera
+        </div>
+      </section>
+
+      <!-- img -->
+      <section>
+        <img src="foto.jpeg" alt="">
+      </section>
+
+      <!-- info about me -->
+      <section>
+        <h1>
+          Junior <br>
+          Full-Stack Web Developer
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt eaque magni dignissimos vero, ex
+          dicta necessitatibus atque voluptatibus aliquam provident natus, ipsum, doloremque porro!
+          Necessitatibus, aliquam? Quis obcaecati veniam dolores?
+        </p>
+      </section>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use "./../styles/general.scss" as *;
+@use "./../styles/partials/variables.scss" as *;
 
 header {
-  height: 150px;
-  background-color: #778899;
+  font-size: 24px;
+  color: #fff;
+  background-color: $background_dark;
+  padding: 50px 150px;
+  display: flex;
+  justify-content: space-between;
+
+  nav {
+    width: 80%;
+    text-align: end;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  a {
+    margin-left: 20px;
+  }
+
+  ul {
+    padding: 0 40px;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-start;
+    gap: 40px;
+  }
+}
+
+// Jumbo
+#hero {
+  color: #778899;
+  height: 100vh;
+  background-color: $background_dark;
+  padding: 60px 0;
+
+  .center_container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+
+    section {
+      min-width: calc(100%/3);
+
+      img {
+        width: 90%;
+        margin: 0 auto;
+      }
+    }
+  }
+
+  #name {
+    font-size: 60px;
+  }
+}
+
+
+// DEBUG
+// header {
+//   border: 1px #1eff00 dashed;
+
+//   div,
+//   nav,
+//   ul {
+//     border: 1px dashed yellow;
+//   }
+// }
+
+// DEBUG 
+section {
+  border: red 1px dotted;
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,67 +8,37 @@ export default {
 
 <template>
     <main>
-        <section class="get_start_today">
-            <div class="center_container">
-                <div class="name">
-                    <!-- <img src="getstarttoday/artist-quote-icon.png" alt="virgolette"> -->
-                    <h1>
-                        Ciao! Io sono Alessandra Martera
-                    </h1>
 
-                </div>
-                <!-- carousel with my title job -->
-                <div class="carousel">
-                    <div class="text">
-                        e sono una junior full Stack Web Developer
-                    </div>
-                </div>
-            </div>
-
-        </section>
     </main>
 </template>
 
 <style lang="scss" scoped>
 @use "./../styles/general.scss" as *;
+@use "./../styles/partials/variables.scss" as *;
 
 main {
-    color: #778899;
-    height: 100vh;
-    background-color: #3c3c3c;
-    padding: 60px 0;
-
-    .center_container {
-        width: 80%;
-        margin: 0 auto;
 
 
-    }
-}
+    // .name {
+    //     display: flex;
+    //     flex-direction: column;
+    //     justify-content: center;
+    //     align-items: center;
+    //     gap: 30px;
+    //     width: 40%;
+    //     text-align: center;
+
+    //     img {
+    //         width: 40px;
+    //     }
+
+    //     h1 {
+    //         font-size: 50px;
+    //     }
+    // }
+
+    // .text {}
 
 
-
-.center_container {
-    display: flex;
-
-    .name {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 30px;
-        width: 40%;
-        text-align: center;
-
-        img {
-            width: 40px;
-        }
-
-        h1 {
-            font-size: 50px;
-        }
-    }
-
-    .text {}
 }
 </style>
